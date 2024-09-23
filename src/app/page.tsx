a1b2a134500c2bdd3b703da1c59d1b3d294ea1b1
OclaZ <aslikhhamza2001@gmail.com>
2024-09-23 02:03:05 +0100

    add next-auth and items table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,48 @@
+import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import SignIn from "@/components/ui/sign-in";
+import { SignOut } from "@/components/ui/sign-out";
 import { database } from "@/db/database";
-import { bids as bidsSchema } from "@/db/schema";
+import { items } from "@/db/schema";
 import { revalidatePath } from "next/cache";
+
 export default async function HomePage() {
-  const bids = await database.query.bids.findMany();
+  const allItems = await database.select().from(items); // Fetch items from the database
+  const session = await auth();
+
+  if (!session || !session.user) return <SignIn />;
+
   return (
     <main className="">
+      <SignOut />
+      <p>Welcome, {session?.user?.name}</p>
+
       <form
         action={async (formData: FormData) => {
           "use server";
-          //const bid = formData.get("bid") as string;
-          await database.insert(bidsSchema).values({});
-          revalidatePath("/");
+          const name = formData.get("name");
+          if (!name) {
+            throw new Error("Name is required");
+          }
+
+          await database.insert(items).values({
+            name: name as string,
+            userId: session?.user?.id,
+          });
+
+          revalidatePath("/"); // Revalidate the path to show the new item
         }}
       >
-        <Input name="bid" placeholder="Bid" />
-        <Button type="submit">Place bid</Button>
+        <Input name="name" placeholder="Name your item" />
+        <Button type="submit">Post item</Button>
       </form>
 
-      {bids.map((bid) => (
-        <div key={bid.id}>{bid.id}</div>
-      ))}
+      <div>
+        {allItems.map((item) => (
+          <div key={item.id}>{item.name}</div>
+        ))}
+      </div>
     </main>
   );
 }
